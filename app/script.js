39aa1a03dc8b5a705339ed8379c6e59e23447f11
Fernando Stadler <fernandostadler2@gmail.com>
2022-05-10 23:48:08 -0300

    Primeira vesão

--- app/script.js
+++ app/script.js
@@ -11,6 +11,7 @@ Tesoura perde pedra
 **/
 
 // CPU Selections 
+var spinner = document.getElementById('load-cpu');
 var cpu_rock = document.getElementById('rocket-cpu');
 var cpu_paper = document.getElementById('paper-cpu');
 var cpu_scissors = document.getElementById('scissors-cpu');
@@ -28,70 +29,127 @@ var cpuPoint = 0
 
 var scorePlayer = document.getElementById('score-player');
 var scoreCpu = document.getElementById('score-cpu');
-var start = document.getElementById('start')
 var reset = document.getElementById('reset')
+var alert = document.getElementById('alert');
 
 function random() {
      const random = Math.random() * 2;
      return Math.round(random)
 }
+function remove(item, cssClass) {
+     item.classList.remove(cssClass);
+}
+function add(item, cssClass) {
+     item.classList.add(cssClass);
+}
+function alertMessage(msg, backgroundAdd) {
+     alert.innerHTML = msg;
+     add(alert, backgroundAdd);
+}
 
+function cleanClass() {
+     alert.innerHTML = "";
+     alert.className = 'alert'
+}
 function playRock() {
      var choiceRock = parseInt(rock_p.value = 1);
-
-     if (choiceRock  && random() == 2) {
+     if (choiceRock && random() == 2) {
+          cleanClass();
+               alertMessage("Você ganhou :D", 'bg-success');
           playerPoint += 1;
           scorePlayer.value = playerPoint;
-          alert(`Você  ganhou, a maquina jogou tesoura e o seu foi pedra | Score: ${playerPoint}`)
-     } else if (choiceRock == 0 && random() == 0) {
-          alert("empatou")
-     }
-     else {
+          remove(cpu_scissors, 'd-none')
+          add(cpu_rock, 'd-none')
+          add(cpu_paper, 'd-none')
+     } else if (choiceRock === 1 && random() === 0) {
+          cleanClass();
+               alertMessage("Empate ¬¬' ", 'bg-warning');
+          add(cpu_scissors, 'd-none')
+          add(cpu_paper, 'd-none')
+          remove(cpu_rock, 'd-none')
+     } else if (choiceRock === 1 && random() === 1) {
+          cleanClass();
+               alertMessage("Você perdeu :(", 'bg-danger');
           cpuPoint += 1;
           scoreCpu.value = cpuPoint;
-         alert(`Você não ganhou, número da maquina foi ${random()} e o seu foi ${choiceRock}`)
+          add(cpu_scissors, 'd-none')
+          add(cpu_rock, 'd-none')
+          remove(cpu_paper, 'd-none')
      }
 }
 
+
 function playPapper() {
      var choicePapper = parseInt(paper_p.value = 2);
-     if (choicePapper && random() === 1) {
-          playerPoint += 1;
-          scorePlayer.value = playerPoint;
-          alert(`Você  ganhou, a maquina jogou pedra e o seu foi papel | Score: ${playerPoint}`)
-     } else {
+     if (choicePapper && random() == 2) {
+          cleanClass(),
+          alertMessage("Você perdeu :(", 'bg-danger');
           cpuPoint += 1;
           scoreCpu.value = cpuPoint;
-         
-          alert(`Você não ganhou, número da maquina foi ${random()} e o seu foi ${choicePapper}`)
+          remove(cpu_scissors, 'd-none')
+          add(cpu_rock, 'd-none')
+          add(cpu_paper, 'd-none')
+     } else if (choicePapper && random() == 0) {
+          cleanClass(),
+          alertMessage("Você ganhou :D", 'bg-success');
+          playerPoint += 1;
+          scorePlayer.value = playerPoint;
+          add(cpu_scissors, 'd-none')
+          add(cpu_paper, 'd-none')
+          remove(cpu_rock, 'd-none')
+     } else if (choicePapper && random() === 1) {
+          cleanClass(),
+          alertMessage("Empate ¬¬' ", 'bg-warning');
+          cpuPoint += 1;
+          scoreCpu.value = cpuPoint;
+          add(cpu_scissors, 'd-none')
+          add(cpu_rock, 'd-none')
+          remove(cpu_paper, 'd-none')
      }
 }
 
 function playScissors() {
      var choiceScissors = parseInt(scissors_p.value = 3);
-
-     if (choiceScissors && random() === 1) {
-          playerPoint += 1;
-          scorePlayer.value = playerPoint;
-          alert(`Você  ganhou, a maquina jogou papel e o seu foi tesoura | Score: ${playerPoint}`)
-     } else {
+     if (choiceScissors && random() == 2) {
+          cleanClass(),
+          alertMessage("Empate ¬¬' ", 'bg-warning');
+          remove(cpu_scissors, 'd-none')
+          add(cpu_rock, 'd-none')
+          add(cpu_paper, 'd-none')
+     } else if (choiceScissors && random() == 0) {
+          cleanClass(),
+          alertMessage("Você perdeu :(", 'bg-danger');
           cpuPoint += 1;
           scoreCpu.value = cpuPoint;
-          alert(`Você não ganhou, número da maquina foi ${random()} e o seu foi ${choiceScissors}`)
+          add(cpu_scissors, 'd-none')
+          add(cpu_paper, 'd-none')
+          remove(cpu_rock, 'd-none')
+     } else if (choiceScissors && random() === 1) {
+          cleanClass(),
+          alertMessage("Você ganhou :D", 'bg-success');
+          playerPoint += 1;
+          scorePlayer.value = playerPoint;
+          add(cpu_scissors, 'd-none')
+          add(cpu_rock, 'd-none')
+          remove(cpu_paper, 'd-none')
      }
 }
 
 function restart() {
+     cleanClass();
+     add(cpu_rock, 'd-none')
+     add(cpu_paper, 'd-none')
+     add(cpu_scissors, 'd-none')
      scorePlayer.value = 0;
      playerPoint = 0;
      cpuPoint = 0;
      scoreCpu.value = 0;
 }
 
-
 rock_p.onclick = playRock;
 paper_p.onclick = playPapper;
 scissors_p.onclick = playScissors;
+// reset.onclick = restart;
 reset.onclick = restart;
 
 
